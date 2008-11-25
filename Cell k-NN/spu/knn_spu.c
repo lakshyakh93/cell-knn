0ce0bf074749b1e273eef6937d072a0c938a194a
dani.winkler <dani.winkler@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-25 21:18:12 +0000

    added additional version

--- Cell k-NN/spu/knn_spu.c
+++ Cell k-NN/spu/knn_spu.c
@@ -66,3 +66,60 @@ int main(unsigned long long id, unsigned long long parm) {
 
 	return EXIT_SUCCESS;
 }
+
+// should also work, uses alternative syntax:
+// mfc commands instead of composite intrinsics
+int main2(unsigned long long id, unsigned long long parm) {
+	int i, j, left, count;
+	unsigned int tagId, tag_mask;
+	
+	distance = 0.0;
+
+	// Reserve a tag ID and create corresponding tag mask.
+	tagId = mfc_tag_reserve();
+	tag_mask = 1 << tagId;
+
+	// TODO why "-1" at spu_writech(MFC_WrTagMask, -1);
+	// => -1 == lots of 1s :), therefore this mask may be combined
+	// with any tagId
+	mfc_write_tag_mask(tag_mask);
+
+	// Input parameter parm is a pointer to the k-NN parameters.
+	// Fetch the context, waiting for it to complete.
+	mfc_get((void *) (&parameters), (unsigned int) parm,
+			sizeof(Parameters), tagId, 0, 0);
+	mfc_read_tag_status_all();
+
+	// For each block of dimensions.
+	for (i = 0; i < parameters.count; i += DIMENSIONS_PER_BLOCK) {
+		// Determine the number of dimensions in this block.
+		left = parameters.count - i;
+		count = (left < DIMENSIONS_PER_BLOCK) ? left : DIMENSIONS_PER_BLOCK;
+		
+		// Fetch the data. Wait for DMA to complete before computation.
+		mfc_get((void *) (query), (unsigned int) (parameters.query + i),
+						count * sizeof(int), tagId, 0, 0);
+		mfc_get((void *) (reference), (unsigned int) (parameters.reference + i), 
+				count * sizeof(int), tagId, 0, 0);
+
+		// Wait for final DMA to complete before terminating SPE thread.
+		mfc_read_tag_status_all();
+
+		// Compute the distance for the block of dimensions.
+		for (j = 0; j < count; j++) {
+			int difference = reference[j] - query[j];
+			distance += difference * difference;
+		}
+	}
+
+	// Put distance data back into main storage.
+	mfc_put((void *) (&distance), (unsigned int) (parameters.distance),
+			sizeof(double), tagId, 0, 0);
+
+	// Wait for final DMA to complete before terminating SPE thread.
+	mfc_read_tag_status_all();
+	
+	printf("(0x%llx) distance = %lf\n", id, distance);
+
+	return EXIT_SUCCESS;
+}
