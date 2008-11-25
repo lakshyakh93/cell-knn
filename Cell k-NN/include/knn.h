4e45e0e8ba331d4827f67ff0fd29abed227ead7b
adrian.marte <adrian.marte@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-25 17:14:42 +0000

    Align all elements of structs.

--- Cell k-NN/include/knn.h
+++ Cell k-NN/include/knn.h
@@ -2,16 +2,16 @@
 #define KNN_H_
 
 typedef struct {
-	int count;
-	int *query;
-	int *reference;
-	double distance;
+	int count __attribute__ ((aligned (16)));
+	int *query __attribute__ ((aligned (16)));
+	int *reference __attribute__ ((aligned (16)));
+	double *distance __attribute__ ((aligned (16)));
 } Parameters;
 
 typedef struct {
-	int label;
-	int dimensions;
-	int *vector;
+	int label __attribute__ ((aligned (16)));
+	int dimensions __attribute__ ((aligned (16)));
+	int *vector __attribute__ ((aligned (16)));
 } Point;
 
 //double distance(Point *query, Point *reference, int dimension);
