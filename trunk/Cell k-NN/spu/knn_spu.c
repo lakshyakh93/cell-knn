5c36620c3427472a912d6612a3ede0b827fc57a6
abroxos <abroxos@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-26 13:08:58 +0000

    Removed redundant main function.

--- trunk/Cell k-NN/spu/knn_spu.c
+++ trunk/Cell k-NN/spu/knn_spu.c
@@ -18,59 +18,6 @@ volatile double distance __attribute__((aligned(16)));
 
 int main(unsigned long long id, unsigned long long parm) {
 	int i, j, left, count;
-	unsigned int tagId;
-	
-	distance = 0.0;
-
-	// Reserve a tag ID.
-	tagId = mfc_tag_reserve();
-
-	spu_writech(MFC_WrTagMask, -1);
-
-	// Input parameter parm is a pointer to the k-NN parameters.
-	// Fetch the context, waiting for it to complete.
-	spu_mfcdma32((void *) (&parameters), (unsigned int) parm,
-			sizeof(Parameters), tagId, MFC_GET_CMD);
-	(void) spu_mfcstat(MFC_TAG_UPDATE_ALL);
-
-	// For each block of dimensions.
-	for (i = 0; i < parameters.count; i += DIMENSIONS_PER_BLOCK) {
-		// Determine the number of dimensions in this block.
-		left = parameters.count - i;
-		count = (left < DIMENSIONS_PER_BLOCK) ? left : DIMENSIONS_PER_BLOCK;
-
-		// Fetch the data. Wait for DMA to complete before computation.
-		spu_mfcdma32((void *) (query), (unsigned int) (parameters.query + i),
-				count * sizeof(int), tagId, MFC_GET_CMD);
-		spu_mfcdma32((void *) (reference), (unsigned int) (parameters.reference + i), 
-				count * sizeof(int), tagId, MFC_GET_CMD);
-		
-		// Wait for final DMA to complete before terminating SPE thread.
-		(void) spu_mfcstat(MFC_TAG_UPDATE_ALL);
-		
-		// Compute the distance for the block of dimensions.
-		for (j = 0; j < count; j++) {
-			int difference = reference[j] - query[j];
-			distance += difference * difference;
-		}
-	}
-
-	// Put distance data back into main storage.
-	spu_mfcdma32((void *) (&distance), (unsigned int) (parameters.distance),
-			sizeof(double), tagId, MFC_PUT_CMD);
-
-	// Wait for final DMA to complete before terminating SPE thread.
-	(void) spu_mfcstat(MFC_TAG_UPDATE_ALL);
-	
-	printf("(0x%llx) distance = %lf\n", id, distance);
-
-	return EXIT_SUCCESS;
-}
-
-// should also work, uses alternative syntax:
-// mfc commands instead of composite intrinsics
-int main2(unsigned long long id, unsigned long long parm) {
-	int i, j, left, count;
 	unsigned int tagId, tag_mask;
 	
 	distance = 0.0;
@@ -79,9 +26,6 @@ int main2(unsigned long long id, unsigned long long parm) {
 	tagId = mfc_tag_reserve();
 	tag_mask = 1 << tagId;
 
-	// TODO why "-1" at spu_writech(MFC_WrTagMask, -1);
-	// => -1 == lots of 1s :), therefore this mask may be combined
-	// with any tagId
 	mfc_write_tag_mask(tag_mask);
 
 	// Input parameter parm is a pointer to the k-NN parameters.
