a546efe91d438e8d84f521021cf66a72ef3cce4f
adrian.marte <adrian.marte@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-26 13:06:33 +0000

    Added comments. Added function classify.

--- trunk/Cell k-NN/include/knn.h
+++ trunk/Cell k-NN/include/knn.h
@@ -2,9 +2,17 @@
 #define KNN_H_
 
 typedef struct {
+	// Number of elements in the query and reference arrays.
 	int count __attribute__ ((aligned (16)));
+
+	// Array of integers representing the vector of the query point.
 	int *query __attribute__ ((aligned (16)));
+
+	// Array of integers representing the vector of the reference point.
 	int *reference __attribute__ ((aligned (16)));
+
+	// Pointer to the distance of the query vector to the reference vector.
+	// The pointer is necessary for DMA transfers from SPE to PPE.
 	double *distance __attribute__ ((aligned (16)));
 } Parameters;
 
@@ -14,7 +22,6 @@ typedef struct {
 	int *vector __attribute__ ((aligned (16)));
 } Point;
 
-//double distance(Point *query, Point *reference, int dimension);
-//void classify(int k, Point *query, Point **training, int length, int dimension);
+void classify(int k, Point *query, Point **training, int length, int dimension);
 
 #endif
