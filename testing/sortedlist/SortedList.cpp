b925b61858de3bd5be117ad58d8d746250e751bc
dani.winkler <dani.winkler@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-28 18:58:31 +0000

    generic oo implementation of sorted list

---
+++ testing/sortedlist/SortedList.cpp
@@ -0,0 +1,104 @@
+#include "SortedList.h"
+
+template <class T> 
+SortedList<T>::~SortedList()
+{
+	node *temp;
+	
+	while(head) {
+		temp = head;
+		head = head->next;
+		delete temp;
+	}
+}
+
+template <class T> 
+int SortedList<T>::getMaxSize() {
+	return maxSize;
+}
+
+template <class T> 
+int SortedList<T>::getCurrSize() {
+	return currSize;
+}
+
+template <class T> 
+bool SortedList<T>::isEmpty() {
+	return getCurrSize() == 0;
+}
+
+template <class T> 
+bool SortedList<T>::isFull() {
+	return getCurrSize() >= getMaxSize();
+}
+
+template <class T> 
+void SortedList<T>::print() {
+	node *temp = head;
+	
+	while(temp) {
+		std::cout << temp->key << " ";
+		temp = temp->next;
+	}
+	std::cout << "\n";
+}
+
+template <class T> 
+int SortedList<T>::insert(int key, T value) {
+	node *bigger, *smaller;
+	
+	if (isFull()) {
+		if (key >= head->key)
+			return 0;
+		
+		--currSize;
+		bigger = head;
+		head = head->next;
+		delete bigger;
+		return insert(key, value);
+	}
+	
+	if (isEmpty()) {
+		head = new node(key, value);
+	} else {
+		bigger = smaller = head;
+		
+		while (smaller && key < smaller->key) {
+			bigger = smaller;
+			smaller = smaller->next;
+		}
+		if (smaller != head)		
+			bigger->next = new node(key, value, smaller);
+		else
+			head = new node(key, value, smaller);
+	}
+	
+	return ++currSize;;
+}
+
+
+// **********************
+// *main fct for testing*
+// **********************
+
+int main() {
+	SortedList<char> mylist(5);
+	mylist.insert(5, '5');
+	mylist.print();
+	mylist.insert(6, '5');
+	mylist.print();
+	mylist.insert(3, '5');
+	mylist.print();
+	mylist.insert(5, '5');
+	mylist.print();
+	mylist.insert(8, '5');
+	mylist.print();
+	mylist.insert(8, '5');
+	mylist.print();
+	mylist.insert(8, '5');
+	mylist.print();
+	mylist.insert(1, '5');
+	mylist.print();
+	mylist.insert(2, '5');
+	mylist.print();
+}
