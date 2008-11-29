4b7849d32cd4a300b571d6b80767af590d6ce5b3
dani.winkler <dani.winkler@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-29 12:39:33 +0000

    added majority vote via std::map

---
+++ testing/majorityvote/MajorityVote.cpp
@@ -0,0 +1,43 @@
+#include "SortedList.cpp"
+#include <map>
+
+int main() {
+	SortedList<char> mylist(5);
+	mylist.insert(5, '2');
+	mylist.print();
+	mylist.insert(6, '1');
+	mylist.print();
+	mylist.insert(3, '3');
+	mylist.print();
+	mylist.insert(5, '1');
+	mylist.print();
+	mylist.insert(8, '3');
+	mylist.print();
+	mylist.insert(8, '2');
+	mylist.print();
+	mylist.insert(8, '2');
+	mylist.print();
+	mylist.insert(1, '2');
+	mylist.print();
+	mylist.insert(2, '1');
+	mylist.print();
+
+
+	std::map<char, int> majorityVote;
+	
+	for (SortedList<char>::Iterator it = mylist.begin(); it != mylist.end(); ++it) {
+		majorityVote[it->value]++;
+	}
+
+	int maxVal = 0;
+	char max;
+	
+	for (std::map<char, int>::iterator it = majorityVote.begin(); it != majorityVote.end(); ++it) {
+		if (it->second > maxVal) {
+			maxVal = it->second;
+			max = it->first;
+		}
+	}
+	
+	std::cout << max << " (votes: " << maxVal << ")\n";
+}
