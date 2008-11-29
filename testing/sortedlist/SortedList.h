12c1c527dd0f8b41752ece005f7bee9e29696181
dani.winkler <dani.winkler@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2008-11-29 12:03:02 +0000

    added iterator

--- testing/sortedlist/SortedList.h
+++ testing/sortedlist/SortedList.h
@@ -3,22 +3,27 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iterator>
 
-template <class T> 
-class SortedList
-{
+template <class T> class SortedList {
 	struct node {
 		int key;
 		T value;
 		node *next;
-		node(int k, T v, node* n) : key(k), value(v), next(n) {}
-		node(int k, T v) : key(k), value(v), next(NULL) {}
+		node(int k, T v, node* n) :
+			key(k), value(v), next(n) {
+		}
+		node(int k, T v) :
+			key(k), value(v), next(NULL) {
+		}
 	};
 	int maxSize;
 	int currSize;
 	node *head;
 public:
-	SortedList(int m) : maxSize(m), currSize(0), head(NULL) {}
+	SortedList(int m) :
+		maxSize(m), currSize(0), head(NULL) {
+	}
 	virtual ~SortedList();
 	int getMaxSize();
 	int getCurrSize();
@@ -26,6 +31,61 @@ public:
 	bool isFull();
 	int insert(int key, T value);
 	void print();
+
+	class Iterator : public std::iterator<std::forward_iterator_tag, T> {
+private:
+		node* nd;
+public:
+		Iterator(node* p) :
+			nd(p) {
+		}
+		~Iterator() {
+		}
+
+		Iterator& operator=(const Iterator& other) {
+			nd = other.nd;
+			return (*this);
+		}
+
+		bool operator==(const Iterator& other) {
+			return (nd == other.nd);
+		}
+
+		bool operator!=(const Iterator& other) {
+			return (nd != other.nd);
+		}
+
+		Iterator& operator++() {
+			if (nd != NULL) {
+				nd = nd->next;
+			}
+			return (*this);
+		}
+
+		Iterator& operator++(int) {
+			Iterator tmp(*this);
+			++(*this);
+			return (tmp);
+		}
+
+		// Return by reference not value (like most container classes do) 
+		node operator*() {
+			return *nd;
+		}
+
+		node* operator->() {
+			return nd;
+		}
+
+	};
+	Iterator begin() {
+		return (Iterator(head));
+	}
+
+	Iterator end() {
+		return (Iterator(NULL));
+	}
+
 };
 
 #endif /*SORTEDLIST_H_*/
