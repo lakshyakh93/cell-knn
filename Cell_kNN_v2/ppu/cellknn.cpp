7b01f60ed5d04b823325c1b4ca88e0cfa9c87a8b
adrian.marte <adrian.marte@344f30e2-9ea5-11dd-93a9-677c3c677ea2>
2009-01-18 11:13:05 +0000

    Introduced function "classify" and moved corresponding code from function "main".

--- Cell_kNN_v2/ppu/cellknn.cpp
+++ Cell_kNN_v2/ppu/cellknn.cpp
@@ -41,7 +41,8 @@ spu_data_t data[MAX_NUM_SPES];
  * @param image Image from which the data comes
  * @param length Size of an image
  */
-void imageToPoint(Point<int, int> *point, unsigned char label, unsigned char *image, int length) {
+void imageToPoint(Point<int, int> *point, unsigned char label,
+		unsigned char *image, int length) {
 	for (int i = 0; i < length; ++i) {
 		point->getValues()[i] = static_cast<int> (image[i]);
 	}
@@ -64,142 +65,62 @@ void *spu_pthread(void *arg) {
 	pthread_exit(NULL);
 }
 
-uint32_t calculate(char *buffer) {
-	int i, temp=0;
-
-	for (i = 0; i < 768*4; i++)
-		temp+=buffer[i];
-
-	printf("PPE sums to %d\n", temp);
-
-	return 0;
-}
-
-
-//============================================================================
-// main
-//============================================================================
-int main() {
+/**
+ * @brief Classifies a set of test points using a set of training points.
+ *
+ * @param k The number of k nearest neighbours.
+ * @param test_points The set of test points.
+ * @param training_points The set of training points.
+ * 
+ * @return An array of calculated labels for the set of test points. 
+ *         The element at the first position represents the calculated 
+ *         label of the first test points. 
+ */
+int *classify(int k, Points<int, int> &test_points,
+		Points<int, int> &training_points) {
 	time_t start_time, end_time;
-	
-	//*******************************************************
-	cout << "---------[Program start]----------" << endl;
-
-	/*	if (argc < 3) {
-	 fprintf(stderr, "Usage: mnist <k> <mnist path> [<nr train images> <nr test images>]\n");
-	 return EXIT_FAILURE;
-	 }
-	 */
-	cb.k = 5; // TODO atoi(argv[1]);
-
-	#ifdef PRINT
-	cout << "k = " << cb.k << endl;
-	#endif
-	string base = "/tmp/images/"; // TODO string base(argv[2]);
-
-	LabelIterator *trainLabels = openLabels(base + "train-labels-idx1-ubyte");
-	ImageIterator *trainImages = openImages(base + "train-images-idx3-ubyte");
-	LabelIterator *testLabels = openLabels(base + "t10k-labels-idx1-ubyte");
-	ImageIterator *testImages = openImages(base + "t10k-images-idx3-ubyte");
-
-	if (trainLabels == 0 || trainImages == 0 || testLabels == 0 || testImages == 0) {
-		cout
-				<< "One or more files couldn't be found! Make sure that following files are in the directory given as argument:\n"
-				<< "\ttrain-labels-idx1-ubyte\n" << "\ttrain-images-idx3-ubyte\n" << "\tt10k-labels-idx1-ubyte\n"
-				<< "\tt10k-images-idx3-ubyte\n" << endl;
-		exit(-1);
-	}
-
-	trainLabels->count = 1000;
-	trainImages->count = 1000;
-	testLabels->count = 10;
-	testImages->count = 10;
-
-	unsigned char label;
-	unsigned char *image;
-
-	Points<int, int> training_points(trainImages->count, trainImages->rows * trainImages->columns);
-	Points<int, int> test_points(testImages->count, testImages->rows * testImages->columns);
-
-	#ifdef PRINT
-	cout << trainImages->count << " " << trainImages->rows*trainImages->columns << endl;
-	cout << "size: " << trainImages->rows*trainImages->columns << "\tcount: " << trainImages->count << endl;
-	#endif
-
-	int i = 0;
-	Point<int, int> *trainPoint;
-	while (hasNextLabel(trainLabels) && hasNextImage(trainImages)) {
-		image = nextImage(trainImages);
-		label = nextLabel(trainLabels);
-
-		trainPoint = training_points.getPoint(i++);
-		imageToPoint(trainPoint, label, image, (trainImages->columns *trainImages->rows));
-
-		#ifdef PRINT
-		if ((i % 1000) == 0)
-			cout << i << " training images loaded." << endl;
-		#endif
-
-		delete trainPoint;
-		free(image);
-	}
-	
-	i = 0;
-	Point<int, int> *testPoint;
-	while (hasNextLabel(testLabels) && hasNextImage(testImages)) {
-		image = nextImage(testImages);
-		label = nextLabel(testLabels);
-
-		testPoint = test_points.getPoint(i++);
-		imageToPoint(testPoint, label, image, (testImages->columns * testImages->rows));
-
-		#ifdef PRINT
-		if ((i % 1000) == 0)
-			cout << i << " test images loaded." << endl;
-		#endif
-
-		delete testPoint;
-		free(image);
-	}
-	//********************************************************
 
 	time(&start_time);
-	
+
+	cb.k = k;
 	cb.values_size = training_points.getVSize();
 	cb.label_size = training_points.getLSize();
-	
+
 	cb.training_dimension = training_points.getDimension();
 	cb.training_count = training_points.getCount();
-	cb.training_data_size = training_points.getCount() * training_points.getVSize();
-	cb.training_points_per_transfer = TRAINING_VALUES_MAX_SIZE / training_points.getVSize();
-	
+	cb.training_data_size = training_points.getCount()
+			* training_points.getVSize();
+	cb.training_points_per_transfer = TRAINING_VALUES_MAX_SIZE
+			/ training_points.getVSize();
+
 	cb.test_dimension = test_points.getDimension();
 	cb.test_count = test_points.getCount();
 	cb.test_data_size = test_points.getCount() * test_points.getVSize();
 	cb.test_points_per_transfer = TEST_VALUES_MAX_SIZE / test_points.getVSize();
-	
-	cb.ea_training_points = (uint64_t) training_points.getValues(0);
-	cb.ea_training_labels = (uint64_t) training_points.getLabel(0);	
-	cb.ea_test_points = (uint64_t) test_points.getValues(0);
-	cb.ea_test_labels = (uint64_t) test_points.getLabel(0);	
 
+	cb.ea_training_points = (uint64_t) training_points.getValues(0);
+	cb.ea_training_labels = (uint64_t) training_points.getLabel(0);
+	cb.ea_test_points = (uint64_t) test_points.getValues(0);
+	cb.ea_test_labels = (uint64_t) test_points.getLabel(0);
 
 	cb.num_spes = spe_cpu_info_get(SPE_COUNT_USABLE_SPES, -1);
 	if (cb.num_spes > MAX_NUM_SPES) {
 		cb.num_spes = MAX_NUM_SPES;
 	}
-	
-#ifdef PRINT
+
+	#ifdef PRINT
 	printf("PPE:\t Num spes = %d\n", cb.num_spes);
-#endif
-	
+	#endif
+
 	uint32_t num;
 
-	printf("PPE:\t Start calculating\n");fflush(stdout);
+	printf("PPE:\t Start calculating\n");
+	fflush(stdout);
 
 	// create SPE context and load SPE program into the SPE context
 	for (num=0; num<cb.num_spes; num++) {
-		if ((data[num].spe_ctx = spe_context_create(SPE_MAP_PS|SPE_CFG_SIGNOTIFY1_OR|SPE_CFG_SIGNOTIFY2_OR, NULL))==NULL) {
+		if ((data[num].spe_ctx = spe_context_create(SPE_MAP_PS
+				|SPE_CFG_SIGNOTIFY1_OR|SPE_CFG_SIGNOTIFY2_OR, NULL))==NULL) {
 			perror("Failed creating context");
 			exit(1);
 		}
@@ -219,7 +140,8 @@ int main() {
 
 	// map SPE's MFC problem state to main storage (get effective address)
 	for (num=0; num<cb.num_spes; num++) {
-		if ((cb.spu_mfc_ctl[num] = (uint64_t)spe_ps_area_get(data[num].spe_ctx, SPE_CONTROL_AREA))==0) {
+		if ((cb.spu_mfc_ctl[num] = (uint64_t)spe_ps_area_get(data[num].spe_ctx,
+				SPE_CONTROL_AREA))==0) {
 			perror("Failed mapping MFC control area");
 			exit(1);
 		}
@@ -227,20 +149,24 @@ int main() {
 			perror("Failed mapping SPU local store");
 			exit(1);
 		}
-		if ((cb.spu_sig1[num] = (uint64_t)spe_ps_area_get( data[num].spe_ctx, SPE_SIG_NOTIFY_1_AREA))==0){
-			perror ("Failed mapping Signal1 area");	exit (1);
+		if ((cb.spu_sig1[num] = (uint64_t)spe_ps_area_get(data[num].spe_ctx,
+				SPE_SIG_NOTIFY_1_AREA))==0) {
+			perror("Failed mapping Signal1 area");
+			exit(1);
 		}
-		if ((cb.spu_sig2[num] = (uint64_t)spe_ps_area_get( data[num].spe_ctx, SPE_SIG_NOTIFY_2_AREA))==0){
-			perror ("Failed mapping Signal2 area");	exit (1);
+		if ((cb.spu_sig2[num] = (uint64_t)spe_ps_area_get(data[num].spe_ctx,
+				SPE_SIG_NOTIFY_2_AREA))==0) {
+			perror("Failed mapping Signal2 area");
+			exit(1);
 		}
 	}
 
-	
 	// send each SPE its number using BLOCKING mailbox write
 	for (num=0; num<cb.num_spes; num++) {
 		// write 1 entry to in_mailbox - we don't know if we have availalbe space so use blocking
 		// cb parameter have to be loaded after receiving local id!!!
-		spe_in_mbox_write(data[num].spe_ctx, (uint32_t*)&num, 1, SPE_MBOX_ALL_BLOCKING);
+		spe_in_mbox_write(data[num].spe_ctx, (uint32_t*)&num, 1,
+				SPE_MBOX_ALL_BLOCKING);
 	}
 
 	// wait for all SPEs to complete
@@ -257,12 +183,116 @@ int main() {
 			exit(1);
 		}
 	}
-	
+
 	time(&end_time);
-	
+
 	double difference = difftime(end_time, start_time);
 	printf("It took %.2lf seconds to calculate %d test points and %d training points\n",
 			difference, cb.test_count, cb.training_count);
+
+	return 0;
+}
+
+//============================================================================
+// main
+//============================================================================
+int main() {
+	//*******************************************************
+	cout << "---------[Program start]----------" << endl;
+
+	/*	if (argc < 3) {
+	 fprintf(stderr, "Usage: mnist <k> <mnist path> [<nr train images> <nr test images>]\n");
+	 return EXIT_FAILURE;
+	 }
+	 */
+	int k = 5; // TODO atoi(argv[1]);
+
+	#ifdef PRINT
+	cout << "k = " << k << endl;
+	#endif
+	
+	string base = "/tmp/images/"; // TODO string base(argv[2]);
+
+	LabelIterator *trainLabels = openLabels(base + "train-labels-idx1-ubyte");
+	ImageIterator *trainImages = openImages(base + "train-images-idx3-ubyte");
+	LabelIterator *testLabels = openLabels(base + "t10k-labels-idx1-ubyte");
+	ImageIterator *testImages = openImages(base + "t10k-images-idx3-ubyte");
+
+	if (trainLabels == 0 || trainImages == 0 || testLabels == 0 || testImages
+			== 0) {
+		cout
+				<< "One or more files couldn't be found! Make sure that following files are in the directory given as argument:\n"
+				<< "\ttrain-labels-idx1-ubyte\n"
+				<< "\ttrain-images-idx3-ubyte\n"
+				<< "\tt10k-labels-idx1-ubyte\n" << "\tt10k-images-idx3-ubyte\n"
+				<< endl;
+		exit(-1);
+	}
+
+	trainLabels->count = 1000;
+	trainImages->count = 1000;
+	testLabels->count = 10;
+	testImages->count = 10;
+
+	unsigned char label;
+	unsigned char *image;
+
+	Points<int, int> training_points(trainImages->count, trainImages->rows
+			* trainImages->columns);
+	Points<int, int> test_points(testImages->count, testImages->rows
+			* testImages->columns);
+
+	#ifdef PRINT
+	cout << trainImages->count << " " << trainImages->rows*trainImages->columns << endl;
+	cout << "size: " << trainImages->rows*trainImages->columns << "\tcount: " << trainImages->count << endl;
+	#endif
+
+	int i = 0;
+	Point<int, int> *trainPoint;
+	while (hasNextLabel(trainLabels) && hasNextImage(trainImages)) {
+		image = nextImage(trainImages);
+		label = nextLabel(trainLabels);
+
+		trainPoint = training_points.getPoint(i++);
+		imageToPoint(trainPoint, label, image, (trainImages->columns
+				*trainImages->rows));
+
+		#ifdef PRINT
+		if ((i % 1000) == 0)
+			cout << i << " training images loaded." << endl;
+		#endif
+
+		delete trainPoint;
+		free(image);
+	}
+
+	i = 0;
+	Point<int, int> *testPoint;
+	while (hasNextLabel(testLabels) && hasNextImage(testImages)) {
+		image = nextImage(testImages);
+		label = nextLabel(testLabels);
+
+		testPoint = test_points.getPoint(i++);
+		imageToPoint(testPoint, label, image, (testImages->columns
+				* testImages->rows));
+
+		#ifdef PRINT
+		if ((i % 1000) == 0)
+			cout << i << " test images loaded." << endl;
+		#endif
+
+		delete testPoint;
+		free(image);
+	}
+	//********************************************************
+	
+	int *result = classify(k, test_points, training_points);
+	
+	// TODO: Analyze.
+	
+	if (result != 0) {
+		free(result);
+	}
 
 	printf("PPE:\t Program finished\n");
 
